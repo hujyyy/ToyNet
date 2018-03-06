871eaa30a092faceb3796c2f4e740b34e2267472
UMJCS <minjie@shanghaitech.edu.cn>
2018-03-06 21:38:18 +0800

    user control between moods

--- proj1/SoundCtrl.java
+++ proj1/SoundCtrl.java
@@ -9,7 +9,7 @@ public class SoundCtrl{
 
   protected boolean running;
   ByteArrayOutputStream out;
-  ByteArrayOutputStream sinout;
+
   public void recordAudio(){
     try {
       final AudioFormat format = getFormat();
@@ -51,9 +51,9 @@ public class SoundCtrl{
 
   }
 
-  public  void playAudio(ByteArrayOutputStream out0) {
+  public  void playAudio() {
     try {
-      byte audio[] = out0.toByteArray();
+      byte audio[] = out.toByteArray();
       // InputStream input =
       //   new ByteArrayInputStream(audio);
       final AudioFormat format = getFormat();
@@ -128,12 +128,11 @@ public class SoundCtrl{
               //line.drain();
 
             }
+            line.drain();
             line.close();
-            System.out.println("hhhh ");
 
           } catch (Exception e) {
-            System.err.println("I/O problems: " + e);
-            System.exit(-1);
+            e.printStackTrace();
           }
         }
       };
@@ -146,37 +145,59 @@ public class SoundCtrl{
     }
   }
 
-//  private static byte[] generateSineWavefreq(int frequencyOfSignal, int seconds) {
-//    // total samples = (duration in second) * (samples per second)
-//
-//    byte[] sin = new byte[seconds * 8000];
-//    double samplingInterval = (double) (8000 / frequencyOfSignal);
-//    System.out.println("Sampling Frequency  : "+8000);
-//    System.out.println("Frequency of Signal : "+frequencyOfSignal);
-//    System.out.println("Sampling Interval   : "+samplingInterval);
-//    for (int i = 0; i < sin.length; i++) {
-//      double angle = (2.0 * Math.PI * i) / samplingInterval;
-//      sin[i] = (byte) (Math.sin(angle) * 127);
-//      //System.out.println("" + sin[i]);
-//    }
-//    return sin;
-//  }
+  private static byte[] generateSineWavefreq(int frequencyOfSignal, int seconds) {
+    // total samples = (duration in second) * (samples per second)
+
+    byte[] sin = new byte[seconds * 8000];
+    double samplingInterval = (double) (8000 / frequencyOfSignal);
+    System.out.println("Sampling Frequency  : "+8000);
+    System.out.println("Frequency of Signal : "+frequencyOfSignal);
+    System.out.println("Sampling Interval   : "+samplingInterval);
+    for (int i = 0; i < sin.length; i++) {
+      double angle = (2.0 * Math.PI * i) / samplingInterval;
+      sin[i] = (byte) (Math.sin(angle) * 127);
+      //System.out.println("" + sin[i]);
+    }
+    return sin;
+  }
 
   public static void main(String[] args) throws IOException,InterruptedException{
+
     System.out.println("---------Program Start---------");
-    System.out.println("Press Enter to start recording...");
-    BufferedReader br = new BufferedReader(new
-            InputStreamReader(System.in));
-//      int temp = br.read();
-    SoundCtrl sc = new SoundCtrl();
-//      System.out.println("---------Record Start---------");
-//      sc.recordAudio();
-//      Thread.sleep(1000*5);
-//      sc.running = false;
-    System.out.println("Press Enter to start playing...");
-    br.read();
-    System.out.println("---------Playing Start---------");
-    sc.playSinAudio();
+    boolean ProgramRun=true;
+    while (ProgramRun) {
+      System.out.println("Choose one mood (1,2,3) ---> 1(Part1) 2(Part2) 3(Exit)...");
+      Scanner scan = new Scanner(System.in);
+      int userChoice = scan.nextInt();
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      SoundCtrl sc = new SoundCtrl();
+      //1->record 2->palyisn 3->exit
+      if (userChoice == 1) {
+        System.out.println("Enter Part 1...");
+        System.out.println("Press Enter to start recording...");
+        int temp = br.read();
+        System.out.println("---------Record Start---------");
+        sc.recordAudio();
+        Thread.sleep(1000 * 5);
+        sc.running = false;
+        System.out.println("Press Enter to start playing...");
+        br.read();
+        System.out.println("---------Playing Start---------");
+        sc.playAudio();
+
+      } else if (userChoice == 2) {
+        System.out.println("Enter Part 2...");
+        System.out.println("Press Enter to start playing...");
+        br.read();
+        System.out.println("---------Playing Sin Sound ---------");
+        sc.playSinAudio();
+
+
+      } else if(userChoice==3){
+        System.out.println("--------Program End ----------");
+        ProgramRun=false;
+      }
+    }
 
 
   }
