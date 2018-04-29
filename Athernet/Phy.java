bde271a6348b44217a53253c081ae9be2a93735e
hujy <hujy@shanghaitech.edu.cn>
2018-04-29 17:10:40 +0800

    updata framework

--- Athernet/Phy.java
+++ Athernet/Phy.java
@@ -4,142 +4,150 @@ import java.util.*;
 import javax.swing.*;
 import java.nio.ByteBuffer;
 
+
+enum Node_status{
+  A,
+  B
+
+};
+
+
 public class Phy{
+
+
+  static dataUtils du = new dataUtils();
+
   static int sampleRate = 48000;
-  static int samples_per_bit = 40;
-  protected boolean Rx_run = true;
-  int preamble_size = 132;
+  static int samples_per_bit = 45;
+  protected boolean Rx_running = true;
   int scale = 32;
+  String pathname = "./input.txt";
+  int data_size = 10000; // in bits
+  final AudioFormat format = new AudioFormat(sampleRate, 8, 1, true, true);
+  Node_status status = Node_status.A;
 
-  byte[] preamble;
-  Runnable RxRunner;
-  Runnable TxRunner;
+  ///
+  static int num_pack = du.num_pack;
+  int pack_data_size = data_size /num_pack *samples_per_bit / du.num_freq;
+  int pack_size =  pack_data_size + du.preamble_size;
+  static int preamble_size = du.preamble_size;
+  ///
 
-  void phy_init(){
+  static Runnable RxRunner;
+  static Runnable TxRunner;
+
+  public void init(){
     RxRunner = new Runnable() {
-        int breaktime = 5;
+        int breaktime = 0;
         //int packlength = num_bit*samples_per_bit/num_pack/2;
-        //byte buffer[] = new byte[preamble.length+packlength+breaktime];
+        byte buffer[] = new byte[pack_size];
         byte zerobuffer[] = new byte[1];
         boolean data_arrive = false;
         int patternlength = 6*samples_per_bit;
 
+        byte[] out_array = new byte[data_size];
+
+        public void writeOut (byte[] writeData){
+          try{
+            Writer output = new BufferedWriter(new FileWriter("test.txt", false));
+            for(int i=0;i<writeData.length;i++){
+              if(writeData[i]==1) output.append("1");
+              else output.append("0");
+            }
+            output.flush();
+          }catch (IOException e){e.printStackTrace();}
+        }
+
+
         public void run() {
-        //     out = new ByteArrayOutputStream();
-        //     running = true;
-        //
-        //     int scale = 50;
-        //     int read_start = 0;
-        //     int pack_id = -1;
-        //     try {
-        //         while (running) {
-        //             if(!data_arrive){
-        //               line.read(zerobuffer,0,1);
-        //               if(Math.abs(zerobuffer[0])<50) continue;
-        //               else data_arrive = true;
-        //             }
-        //             int count = line.read(buffer, read_start, buffer.length-read_start);
-        //             if (count > 0) out.write(buffer, 0, count);
-        //
-        //             read_start = 265;//find_start(buffer,preamble,packlength,scale);
-        //
-        //             if(read_start<0){
-        //               //if read_start<0, then we should do a shifting and find the start point from scratch
-        //               read_start = -read_start;
-        //               System.out.print("Need to shift backwards, padding from "+read_start+"/"+buffer.length+"\n");
-        //               continue;
-        //             }else if(read_start>0){
-        //               //if read_start>0 we just find the start_index
-        //                 System.out.print("find the start at "+read_start+"\n");
-        //                 decoding(packlength,buffer,read_start);
-        //                 pack_id++;
-        //                 System.out.print("pack "+pack_id+" process done\n");
-        //                 if(num_pack==pack_id+1) break;
-        //
-        //           }
-        //           if(read_start==0) System.out.print("Did not find the start\n");
-        //
-        //           read_start = 0;
-        //         }
-        //         byte tmpbuffer[] = out.toByteArray();
-        //         out.close();
-        //         // analysis
-        //         System.out.print(tmpbuffer.length);
-        //         ByteArrayInputStream temp = new ByteArrayInputStream(tmpbuffer);
-        //         AudioInputStream temp1 = new AudioInputStream(temp,format,tmpbuffer.length);
-        //         AudioSystem.write(temp1,AudioFileFormat.Type.WAVE,new File("out.wav"));
-        //
-        //         System.out.println("------------->");
-        //
-        //         //for(int i=0;i<tmpbuffer.length;i++) if(tmpbuffer[i]>1) System.out.print(10*tmpbuffer[i]+"\n");
-        //         //decoding(num_pack,num_bit,samples_per_bit,tmpbuffer);
-        //
-        //
-        //
-        //     //     // end of analysis
-        //     } cagdtch (IOException e) {
-        //          System.err.println("I/O problems: " + e);
-        //          System.exit(-1);
-        //      }
-         }
+          System.out.print("Rx starts--------------\n");
+          ByteArrayOutputStream out = new ByteArrayOutputStream();
+          Rx_running = true;
+
+          int read_start = 0;
+          int pack_id = -1;
+
+          //final AudioFormat format = getFormat();
+
+              try {
+                DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+                final TargetDataLine line = (TargetDataLine) AudioSystem.getLine(info);
+                line.open(format);
+                line.start();
+                  while (Rx_running) {
+                      if(!data_arrive){
+                        line.read(zerobuffer,0,1);
+                        if(Math.abs(zerobuffer[0])<50) continue;
+                        else data_arrive = true;
+                      }
+                      int count = line.read(buffer, read_start, buffer.length-read_start);
+                      if (count > 0) out.write(buffer, 0, count);
+                      read_start = preamble_size;//du.find_start(buffer);
+                      System.out.print("read start"+read_start+"\n");
+                      if(read_start<0){
+                        //if read_start<0, then we should do a shifting and find the start point from scratch
+                        read_start = -read_start;
+                        System.out.print("Need to shift backwards, padding from "+read_start+"/"+buffer.length+"\n");
+                        continue;
+                      }else if(read_start>0){
+                        //if read_start>0 we just find the start_index
+                          System.out.print("find the start at "+read_start+"\n");
+                          pack_id++;
+                          int ret  = du.decode_pack(buffer,read_start,out_array,pack_id*data_size/num_pack);
+                          System.out.print("pack "+pack_id+" process done\n");
+                          if(du.num_pack==pack_id+1) break;
+
+                    }
+                    if(read_start==0) System.out.print("Did not find the start\n");
+
+                    read_start = 0;
+                  }
+                  System.out.print("packid "+pack_id+" \n");
+                  byte tmpbuffer[] = out.toByteArray();
+                  out.close();
+                  // analysis
+                  System.out.print(tmpbuffer.length);
+                  ByteArrayInputStream temp = new ByteArrayInputStream(tmpbuffer);
+                  AudioInputStream temp1 = new AudioInputStream(temp,format,tmpbuffer.length);
+                  AudioSystem.write(temp1,AudioFileFormat.Type.WAVE,new File("out.wav"));
+
+                  System.out.println("------------->");
+
+              } catch (Exception e) {
+                   System.err.println("I/O problems: " + e);
+                   System.exit(-1);
+               }
+          System.out.print("Rx ends--------------\n");
+          writeOut(out_array);
+        }
     };
     TxRunner = new Runnable(){
+      int ctr=-1;
 
-
-
-        public void run(){
-
-
-
-
-
+      public void run(){
+        System.out.print("Tx starts--------------");
+        try{
+          if(ctr==-1){
+          byte[] soundarray = du.generate_all(pathname);
+          final AudioFormat txtformat = new AudioFormat(sampleRate, 8, 1, true, true);
+          SourceDataLine txtline = AudioSystem.getSourceDataLine(txtformat);
+          txtline.open(txtformat);
+          txtline.start();
+          txtline.write(soundarray,0,soundarray.length);
+          txtline.drain();
+          txtline.close();
         }
-    };
-
-
-  }
-
-
-
-  public void preamble_init(){
-    preamble = new byte [preamble_size*2];
-
-    double frequencyOfSignal3 = 5000.0; // prenmble frequency 15000hz 5000hz 15000hz
-    double frequencyOfSignal4 = 15000.0;
-
-    double samplingInterval3 = (double) (sampleRate/frequencyOfSignal3);
-    double samplingInterval4 = (double) (sampleRate/frequencyOfSignal4);
-
-    for(int i =0;i<preamble.length;i=i+2){
-
-        double angle3 = (2.0 * Math.PI * i) / samplingInterval3;
-        double angle4 = (2.0 * Math.PI * i) / samplingInterval4;
-        if (i>=preamble.length/3 && i < preamble.length/3*2){
-            System.out.print(scale*Math.sin(angle3)+" ");
-            preamble[i] = (byte)(scale*Math.sin(angle3));
-            System.out.print(preamble[i]+"\n");
-
-        }
-        else {
-          System.out.print(scale*Math.sin(angle4)+" ");
-          preamble[i] = (byte)(scale*Math.sin(angle4));
-          System.out.print(preamble[i]+"\n");
-        }
+      }catch (Exception e) {e.printStackTrace();}
+      System.out.print("Tx ends--------------");
 
     }
-
-  }
-
-
+  };
+}
 
 
-  public int correlation(byte[] array0,byte[] array1,int begin0,int begin1,int length){
-    int result = 0;
-    for (int i = 0; i<length;i++){
-      result+=array0[begin0+i]*array1[begin1+i];
-    }
-    return result;
-  }
+
+
+
 
 
 
@@ -148,9 +156,14 @@ public class Phy{
   public static void main(String[] args) throws IOException,InterruptedException{
     Phy phy = new Phy();
     System.out.println("---------Program Start---------");
-    phy.preamble_init();
+    phy.init();
     String a ="01010101";
     System.out.print(a.substring(0,2));
+    Thread RxThread = new Thread(phy.RxRunner);
+    Thread TxThread = new Thread(phy.TxRunner);
+    RxThread.start();
+    TxThread.start();
+
 
 
 
--- Athernet/dataUtils.java
+++ Athernet/dataUtils.java
@@ -8,16 +8,20 @@ import java.nio.ByteBuffer;
 public class dataUtils{
 
     static int sampleRate = 48000;
-    static int samples_per_bit = 48;
+    static int samples_per_bit = 45;
     protected boolean Rx_run = true;
-    int preamble_size = 3 * samples_per_bit;
+    int preamble_size = 1 * samples_per_bit;
     int scale = 32;
+
     int fft_winsize = 32;
     int num_pack = 100;
+    int num_freq = 5;
+    int pack_data_size = 10000 /num_pack * samples_per_bit / num_freq;
+    int pack_size = preamble_size + pack_data_size;
 
 
 
-    byte[] preamble;
+    byte[] preamble = new byte[preamble_size];
 
 
 
@@ -38,40 +42,47 @@ public class dataUtils{
             line = txtbr.readLine(); // 一次读入一行数据
         }
         int total_input_size = result.length();  // read file total_input_size = 10000 bits
-        int pack_data_size = total_input_size / num_pack;  // 10000 bits / 100 bags = 100
-        int pack_size = preamble_size + pack_data_size * samples_per_bit; // pack_size = preamble + pure_data per bag * sample per bit
+        int pack_data_size_raw = total_input_size / num_pack;  // 10000 bits / 100 bags = 100
+        pack_data_size = pack_data_size_raw*samples_per_bit/num_freq;
+        pack_size = preamble_size + pack_data_size; // pack_size = preamble + pure_data per bag * sample per bit
         System.out.print(pack_size);
-        int total_output_size = total_input_size * samples_per_bit + preamble_size * num_pack;
+        int total_output_size = pack_size*num_pack;
         byte[] output = new byte[total_output_size];
 
         for(int i=0;i<num_pack;i++){
             int outbegin = i*pack_size;
             int outend = (i+1)*pack_size;
-            String sub_result = result.substring(i*pack_data_size,(i+1)*pack_data_size);
-
-            //System.out.print(sub_result.length()+"\n");
+            String sub_result = result.substring(i*pack_data_size_raw,(i+1)*pack_data_size_raw);
             generate_pack(sub_result,output,outbegin,outend);
 
         }
-
+        System.out.print("soundarray length: "+output.length+" \n");
         return output;
 
     }
 
     public void generate_pack(String input,byte[] output,int outbegin,int outend){
         System.out.print("generate pack  No. "+ outbegin/(outend-outbegin) +" -----> "+ input.length() +" \n" );
-
         int pack_size = outend - outbegin;
         int index = outbegin;
         int input_index = 0;
         byte inputarray[] = input.getBytes();
         double frequencyOfSignal1 = sampleRate/fft_winsize*1;
-        double frequencyOfSignal2 = sampleRate/fft_winsize*7;
+        double frequencyOfSignal2 = sampleRate/fft_winsize*3;
+        double frequencyOfSignal3 = sampleRate/fft_winsize*5;
+        double frequencyOfSignal4 = sampleRate/fft_winsize*7;
+        double frequencyOfSignal5 = sampleRate/fft_winsize*9;
+
 
         double samplingInterval1 = (double) (sampleRate/frequencyOfSignal1);
         double samplingInterval2 = (double) (sampleRate/frequencyOfSignal2);
+        double samplingInterval3 = (double) (sampleRate/frequencyOfSignal3);
+        double samplingInterval4 = (double) (sampleRate/frequencyOfSignal4);
+        double samplingInterval5 = (double) (sampleRate/frequencyOfSignal5);
+
+
         for(int k=0;k<preamble_size;k++) output[index++] = (byte)preamble[k];
-        for(int i = 0;i<input.length();i+=2){
+        for(int i = 0;i<input.length();i+=num_freq){
 
             if (inputarray[input_index] ==49){
                 for (int k = 0;k<samples_per_bit;k++){
@@ -87,10 +98,149 @@ public class dataUtils{
                     output[index-samples_per_bit+k] += (byte) (scale * Math.sin(angle2));
                 }
             }
-            input_index+=2;
+            if(inputarray[input_index+2] ==49){
+                for (int k = 0;k<samples_per_bit;k++){
+                    double angle3 = (2.0 * Math.PI * k) / samplingInterval3;
+                    output[index-samples_per_bit+k] += (byte) (scale * Math.sin(angle3));
+                }
+            }
+            if(inputarray[input_index+3] ==49){
+                for (int k = 0;k<samples_per_bit;k++){
+                    double angle4 = (2.0 * Math.PI * k) / samplingInterval4;
+                    output[index-samples_per_bit+k] += (byte) (scale * Math.sin(angle4));
+                    //System.out.print(output[index-samples_per_bit+k]+" \n");
+
+                }
+            }
+            if(inputarray[input_index+4] ==49){
+                for (int k = 0;k<samples_per_bit;k++){
+                    double angle5 = (2.0 * Math.PI * k) / samplingInterval5;
+                    output[index-samples_per_bit+k] += (byte) (scale * Math.sin(angle5));
+                    //System.out.print(output[index-samples_per_bit+k]+" \n");
+
+                }
+            }
+            input_index+=num_freq;
         }
     }
 
+
+    public int find_start(byte[] tmpbuffer){
+      //looking for the starting point
+      int inner_prod;
+      int start_index=0;
+      //magic parameters
+      double power=0;
+      double lowerbound = 100*scale;
+      double local_max = 0;
+
+      int relaxion = 0; //in case that start_index is always zero
+
+      for(int i=0;i<tmpbuffer.length-preamble_size;i++){
+        /*if the preamble is partialy included in the current buffer,
+         we do a shift operation and recall the function outside*/
+        if(start_index+preamble_size>=tmpbuffer.length){
+           int shiftlength = start_index-preamble_size;
+
+           for(int m = shiftlength;m<tmpbuffer.length;m++) tmpbuffer[m-shiftlength+relaxion] = tmpbuffer[m];
+           return -(tmpbuffer.length-shiftlength+relaxion); //take the reverse value to call find_start again after shifting
+        }
+
+            inner_prod = 0;
+            inner_prod = correlation(tmpbuffer,preamble,i,0,preamble_size);
+            inner_prod = inner_prod/200*128;
+
+                    // if(inner_prod>lowerbound){
+                    //   System.out.print("  index--  ");
+                    //   System.out.print(i+patternlength);
+                    //   System.out.print("inner product--  ");
+                    //   System.out.print(inner_prod);
+                    //   System.out.print("  power--  ");
+                    //   System.out.print(power);
+                    //   System.out.print("  raw data--  ");
+                    //   System.out.print(tmpbuffer[i+patternlength]);
+                    //   System.out.print("\n");
+                    //  }
+            power = power/64*63+tmpbuffer[i+preamble_size]*tmpbuffer[i+preamble_size]/64;
+            if(inner_prod>2*power && inner_prod>local_max && inner_prod>lowerbound ) {
+                start_index = i + preamble_size;
+                local_max = inner_prod;
+             }
+           if(start_index!=0&&i-start_index>preamble_size*2/3) {
+             System.out.print(start_index+"\n");
+             if(start_index+pack_data_size>tmpbuffer.length){
+               int shiftlength = start_index-preamble_size;
+               for(int m = shiftlength;m<tmpbuffer.length;m++) tmpbuffer[m-shiftlength+relaxion] = tmpbuffer[m];
+               return -(tmpbuffer.length-shiftlength+relaxion);
+             }
+             return start_index;
+           }
+      }
+      return 0;
+
+    }
+
+    public int decode_pack(byte[] tmpbuffer,int start_index,byte[] out_array,int outbegin){
+
+      fft_test ft = new fft_test();
+       // System.out.println("inside decode\n start "+start_index);
+       //
+       // System.out.print("bufferlength "+tmpbuffer.length);
+
+       int winsize = 32;
+       int truncate = (samples_per_bit-winsize)/2;
+       int stop_index = start_index + pack_data_size;
+        //System.out.print(" stop_index "+stop_index+"\n");
+
+
+       byte[] output = new byte[pack_data_size];
+
+       double freq[] = new double[winsize];
+
+       int out_index=outbegin;
+       for(int i=start_index;i<stop_index;i=i+samples_per_bit){
+
+         ft.getfreq(tmpbuffer,i+truncate,winsize,winsize,freq);
+          if(freq[1]>670) {
+              //System.out.print("2: "+freq[2]+"\n");
+              out_array[out_index++] = 1;
+            }
+          else  out_array[out_index++] = 0;
+
+
+          if(freq[3]>500) {
+              //System.out.print("10: "+freq[10]+"\n");
+              out_array[out_index++] = 1;
+            }
+          else out_array[out_index++] = 0;
+
+          if(freq[5]>390) {
+              //System.out.print("10: "+freq[10]+"\n");
+              out_array[out_index++] = 1;
+            }
+          else out_array[out_index++] = 0;
+
+
+          if(freq[7]>365) {
+              //System.out.print("10: "+freq[10]+"\n");
+              out_array[out_index++] = 1;
+            }
+          else out_array[out_index++] = 0;
+
+          if(freq[9]>350) {
+              //System.out.print("10: "+freq[10]+"\n");
+              out_array[out_index++] = 1;
+            }
+          else out_array[out_index++] = 0;
+
+          }
+       //System.out.print("outindex "+out_index+"\n");
+       //output.flush();
+
+
+       return -1;
+
+    }
 
     public void preamble_init(){
       preamble = new byte [preamble_size];
@@ -121,8 +271,20 @@ public class dataUtils{
 
     }
 
+    public int correlation(byte[] array0,byte[] array1,int begin0,int begin1,int length){
+      int result = 0;
+      for (int i = 0; i<length;i++){
+        result+=array0[begin0+i]*array1[begin1+i];
+      }
+      return result;
+    }
 
 
+
+    public dataUtils(){
+      preamble_init();
+
+    }
     public static void main(String[] args) throws IOException,InterruptedException{
       dataUtils du = new dataUtils();
       System.out.println("---------Program Start---------");
